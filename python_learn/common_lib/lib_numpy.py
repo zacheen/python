2153948aab23d76f97f2f73b3bc94ce37a1bcb72
zacheen <zacheenoao@gmail.com>
2022-07-05 22:04:06 +0800

    [筆記] np 不能接受其他 type

--- python_learn/common_lib/lib_numpy.py
+++ python_learn/common_lib/lib_numpy.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 # #------------------------------------
-# 初始化
-array = np.zeros( shape = (3, 5) , dtype=int ) 
-    # shape = (column, row)
-    # dtype : float int
-print(array)
+# # 初始化
+# array = np.zeros( shape = (3, 5) , dtype=int ) 
+#     # shape = (column, row)
+#     # dtype : float int
+# print(array)
+
+# # 初始值
+# array.fill(100)
+# print(array)
+
+# # # 初始值 一定要是數字
+# # import math
+# # array.fill(math.inf)
+# # print(array)
+
+# # # 裡面不能存 List
+# # array = np.zeros( shape = (1, 1) , dtype=list ) # 結果跟dtype=int一樣
+# # print(array)
