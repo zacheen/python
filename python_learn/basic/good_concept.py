fb0a216ee338cc7a5d1435c7cd727ca68d0c716c
thomasshih <thomasshih@mdamo.com>
2022-07-11 09:49:04 +0800

    [筆記] help

---
+++ python_learn/basic/good_concept.py
@@ -0,0 +1,11 @@
+########################################
+# # << help >>
+# # 印出 def 底下的 string 文字 (通常會是說明文檔)
+def test_help(x):
+    """
+    this is help description
+    line2
+    """
+    return None
+
+help(test_help)
