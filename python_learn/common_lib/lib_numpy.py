bcedbb7afb4790ffd224dedfa27a99d2ade290d3
zacheen <zacheenoao@gmail.com>
2022-07-15 17:13:17 +0800

    [筆記] np自己的數值(nan, inf), 排序(sort), 平均(mean), 對數(log)

--- python_learn/common_lib/lib_numpy.py
+++ python_learn/common_lib/lib_numpy.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-# #------------------------------------
+import math
+# # ------------------------------------
 # # 初始化 方法1 全部都是0
 # array = np.zeros( shape = (3, 5) , dtype=int ) 
 #     # shape = (column, row)
@@ -16,7 +16,8 @@ import numpy as np
 # print(array)
 # # 初始化 方法4 隨機
 # array = np.random.rand(3,4) # 隨機產生 0~1 之間的數字
-array = np.random.randint(1,10,(3,4)) # 隨機產生 0~1 之間的數字
+array = np.random.randint(1,11,(3,4)) # 隨機產生 0~1 之間的數字
+array = array.astype("float16") # 有時候會需要 float 才能計算
 # numpy.random.randint(low, high=None, size=None, dtype='l')
 print(array)
 
@@ -35,6 +36,20 @@ print(array)
 # 取消印出長度限制
 np.set_printoptions(threshold=np.inf)
 
+# # # << 自己的數值格式 >> 
+# # 基本上 np.XX 的數值都只有 float 可以用
+# val_float = np.zeros( 1, dtype=float ) 
+# # np.nan 空值
+# val_float[0] = np.nan
+# print("nan :",val_float)
+# val_float[0] = None
+# print("None :",val_float)
+# # 無限大 無限小
+# val_float[0] = np.inf
+# print("np.inf :",val_float)
+# val_float[0] = -np.inf
+# print("-np.inf :",val_float)
+
 # # 轉換型態、改變型態 (轉型) 
 # # 但是 大轉小會出問題 EX int32 轉 int16
 # new_array = array.astype("float16")
@@ -48,8 +63,27 @@ np.set_printoptions(threshold=np.inf)
 # print("max pos :", np.unravel_index(np.argmax(array),array.shape))
 # # 找出某個項目的位置
 # print("where :",np.where(array==8))
+# where 的其他應用 : 更改指定數值成另一個數值
+# pass_arr = np.array([np.inf, -np.inf, math.inf, -math.inf])
+# print( np.where( pass_arr == np.inf, np.nan, pass_arr) )
+# # 排序 
+# print("sort :\n",np.sort(array))
+# # 平均 
+# print("mean :",np.mean(array))
+# # 平均 (nan 的值會去掉 )
+# array[0] = np.nan
+# print("mean :",np.nanmean(array))
 
-#  << 維度相關 >>
+# << 對每個項目做運算 >>
+
+# # 取對數 log
+# # log 預設的底數是 e 
+# print("log :\n",np.log(array))
+# ret = np.log(np.array([0,1,math.e,10,math.e**2]))
+# print("log 特殊數字 :",ret)
+
+
+#  << 維度相關 >> ####################################################
 # # 變成一維 
 # print("flatten :",array.flatten())
 
@@ -60,7 +94,7 @@ np.set_printoptions(threshold=np.inf)
 # print(array_squ.shape)
 
 
-# # << 較不常用到 >>
+# # << 較不常用到 >> ####################################################
 # # < 百分位數 percentile >
 #     # 求數列中 第 X%分位 的數值
 # # 不管前面的 list 丟進去有沒有按照順序 都沒差
@@ -91,7 +125,7 @@ np.set_printoptions(threshold=np.inf)
 # print("bef clip :",ll)
 # print("aft clip :",np.clip(ll,3,7))
 
-# # # << 注意事項 >>
+# # # << 注意事項 >>  ####################################################
 # # # numpy 的 type 一定要是數字
 # # import math
 # # array.fill(math.inf)
