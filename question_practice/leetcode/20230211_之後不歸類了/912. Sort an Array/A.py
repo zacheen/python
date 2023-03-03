96f873c68e86c76d8bda3711ee380a4a71a84fe2
zacheen <zacheenoao@gmail.com>
2023-03-03 18:04:11 +0800

    [題目練習]

---
+++ question_practice/leetcode/20230211_之後不歸類了/912. Sort an Array/A.py
@@ -0,0 +1,26 @@
+# my 先測試一下 sort 的速度 Beats 92.48%
+class Solution(object):
+    def sortArray(self, nums):
+        nums.sort()
+        return nums
+
+# my 
+# 比 sort 還慢...
+from collections import Counter
+class Solution(object):
+    def sortArray(self, nums):
+        c = Counter(nums)
+        ans = []
+        for this_num in range(-50000, 50001):
+            cou = c[this_num]
+            for i in range(cou) :
+                ans.append(this_num)
+        return ans
+
+# given ans
+
+s = Solution()
+print(s.sortArray())
+
+
+
