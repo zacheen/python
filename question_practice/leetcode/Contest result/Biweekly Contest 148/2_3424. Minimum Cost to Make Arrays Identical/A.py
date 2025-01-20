7edbd6c877fedcc45425909734abe3465959284a
zacheen <zacheenoao@gmail.com>
2025-01-20 19:41:35 +0800

    [Algorithm] minmum diff of two array

---
+++ question_practice/leetcode/Contest result/Biweekly Contest 148/2_3424. Minimum Cost to Make Arrays Identical/A.py
@@ -0,0 +1,21 @@
+# 3424. Minimum Cost to Make Arrays Identical
+# https://leetcode.com/problems/minimum-cost-to-make-arrays-identical/description/
+
+from typing import List
+import functools
+
+# fail : did see "into any number" ...
+# my 229ms Beats100.00%
+class Solution:
+    def minCost(self, arr: List[int], brr: List[int], k: int) -> int:
+        ans = sum(abs(n1-n2) for n1,n2 in zip(arr, brr))
+        arr.sort()
+        brr.sort()
+        ans2 = sum(abs(n1-n2) for n1,n2 in zip(arr, brr)) + k
+        return min(ans, ans2)
+
+s = Solution()
+print("ans :",s.minCost(arr = [-7,9,5], brr = [7,-2,-5], k = 2)) # 13
+
+
+
