181bf4ed6c81d56e16281b0372485581dca1542d
zacheen <zacheenoao@gmail.com>
2025-01-22 21:21:41 +0800

    [Algorithm] permutation and combination, affect range

---
+++ python_learn/Algorithm_template/permu_combi_MOD.py
@@ -0,0 +1,55 @@
+MAX_n = 10
+MAX_PICK = 5
+MOD = 10**9+7
+
+# method 1 #########################################################
+# 適合稀疏取用
+class Factorial:
+    def __init__(self, N, MOD) -> None:
+        N += 1
+        self.MOD = MOD
+        self.fact = [1]*N      # mul from 1 to i
+        self.invfact = [1]*N   
+        for i in range(1, N):
+            self.fact[i] = self.fact[i - 1] * i % self.MOD
+        self.invfact[-1] = pow(self.fact[-1], MOD - 2, MOD)
+        for i in range(N - 2, -1, -1):
+            self.invfact[i] = self.invfact[i + 1] * (i + 1) % self.MOD
+
+    def fac(self, n):
+        return self.fact[n]
+
+    def fac_inv(self, n):
+        return self.invfact[n]
+
+    def combi(self, n, m):
+        if m < 0 or n < m: return 0
+        return self.fact[n] * self.invfact[m] % self.MOD * self.invfact[n - m] % self.MOD
+
+    def permu(self, n, m):
+        if m < 0 or n < m: return 0
+        return self.fact[n] * self.invfact[n - m] % self.MOD
+
+    def catalan(self, n):
+        return (self.combi(2 * n, n) - self.combi(2 * n, n - 1)) % self.MOD
+
+    def inv(self, n):
+        return self.fact[n-1] * self.invfact[n] % self.MOD
+
+# method 2 #########################################################
+# 適合緊密取用
+n = MAX_n
+k = MAX_PICK # 最多從全部的項目中娶幾個(如果不知道可以直接帶n)
+combi = [[0] * (k + 1) for i in range(n + 1)]
+for i in range(n + 1):
+    combi[i][0] = 1
+    for j in range(1, min(i, k) + 1):
+        combi[i][j] = (combi[i - 1][j - 1] + combi[i - 1][j]) % MOD
+
+# test ########################################################
+fact = Factorial(MAX_n, MOD)
+for from_total in range(MAX_n) :
+    for pick_n in range(MAX_PICK) :
+        if combi[from_total][pick_n] != fact.combi(from_total, pick_n) :
+            print("not the same :",combi[from_total][pick_n], fact.combi(from_total, pick_n))
+print("finish")
---
+++ python_learn/Algorithm_template/special/affect_range.py
@@ -0,0 +1,25 @@
+
+arr = [4,1,3,2,5,0,7]
+print(arr)
+
+len_arr = len(arr)
+left = [0]*len_arr
+right = [0]*len_arr
+stack = []
+for i, now_n in enumerate(arr):
+    while stack and arr[stack[-1]] > now_n:
+        stack.pop()
+    left[i] = i - (stack[-1] if stack else -1) - 1
+    stack.append(i)
+stack = []
+for i, now_n in zip(range(len_arr-1, -1, -1), arr[::-1]):
+    while stack and arr[stack[-1]] >= now_n:
+        stack.pop()
+    right[i] = (stack[-1] if stack else len_arr) - i - 1
+    stack.append(i)
+
+print(left)
+print(right)
+i = 3
+print("index",i,"是範圍左邊",left[i],"個與右邊",right[i],"個之間的最小值")
+print("也就是 index", i-left[i], "~", i+right[i], "之間的最小值")
