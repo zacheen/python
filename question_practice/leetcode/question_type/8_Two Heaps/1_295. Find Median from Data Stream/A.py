6d43ea5f6e94963ff5a06ec22540ba3950185342
zacheen <zacheenoao@gmail.com>
2025-03-24 17:41:07 +0800

    [Algorithm] Two heaps

--- question_practice/leetcode/question_type/8_Two Heaps/1_295. Find Median from Data Stream/A.py
+++ question_practice/leetcode/question_type/8_Two Heaps/1_295. Find Median from Data Stream/A.py
@@ -1,52 +1,27 @@
-# my Runtime: 1436 ms, faster than 17.42% of Python3
-# O(nlogn) # 每次插入 O(logn) * n次
+#295. Find Median from Data Stream
+# https://leetcode.com/problems/find-median-from-data-stream/
+
+# my practice again : 112ms Beats90.07%
+from heapq import heappushpop, heappush
 class MedianFinder:
     def __init__(self):
-        self.arr = []
+        self.heap1 = []
+        self.heap2 = []
 
-    def addNum(self, num: int):
-        bisect.insort_right(self.arr, num)
-        # self.arr.append(num)
-
-    def findMedian(self):
-        # print(self.arr)
-        if len(self.arr) % 2 == 0 :
-            mid_indx = len(self.arr)//2
-            return (self.arr[mid_indx]+self.arr[mid_indx-1])/2
+    def addNum(self, num: int) -> None:
+        if len(self.heap1) > len(self.heap2) :
+            # need to put into heap2
+            if self.heap1 and num > self.heap1[0] :
+                num = heappushpop(self.heap1, num)
+            heappush(self.heap2, -num)
         else :
-            return self.arr[len(self.arr)//2]
+            # need to put into heap1
+            if self.heap2 and num < -self.heap2[0] :
+                num = -heappushpop(self.heap2, -num)
+            heappush(self.heap1, num)
 
-# given ans
-# Runtime: 544 ms, faster than 93.70% of Python3 
-# 奇怪 我感覺時間複雜度沒有差這麼多啊 ?
-# O(nlogn)
-class MedianFinder:
-    def __init__(self):
-        self.maxHeap = []
-        self.minHeap = []
-
-    def addNum(self, num: int):
-        if not self.maxHeap or num <= -self.maxHeap[0]:
-            heapq.heappush(self.maxHeap, -num)
-        else:
-            heapq.heappush(self.minHeap, num)
-
-        # balance two heaps s.t.
-        # |maxHeap| >= |minHeap| and |maxHeap| - |minHeap| <= 1
-        # 不能要取的時候再平衡 要不然 maxHeap[0] 可能兩邊數量會很偏
-        # 造成要取的時候 要一直搬移
-        if len(self.maxHeap) < len(self.minHeap):
-            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
-        elif len(self.maxHeap) - len(self.minHeap) > 1:
-            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
-
-    def findMedian(self):
-        if len(self.maxHeap) == len(self.minHeap):
-            return (-self.maxHeap[0] + self.minHeap[0]) / 2.0
-        return -self.maxHeap[0]
-
-s = Solution()
-print(s.())
-
-
-
+    def findMedian(self) -> float:
+        if len(self.heap1) == len(self.heap2) :
+            return (self.heap1[0] - self.heap2[0])/2
+        else :
+            return self.heap1[0]
