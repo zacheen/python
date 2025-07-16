6a76a459da0dca898e9bd189247eb5b68ced0888
zacheen <zacheenoao@gmail.com>
2025-07-16 11:30:03 +0800

    [Algorithm] identify all prime numbers to MAX

--- python_learn/Algorithm_template/special/generating_all_factors.py
+++ python_learn/Algorithm_template/special/generating_all_factors.py
@@ -1,3 +1,4 @@
+# all factors, all divisors
 # generating all the factors of every nums : O(nlogn)
 # this part have to put outside the function become static, preventing recalculate
 MAX = 10 ** 5 + 1
@@ -6,10 +7,26 @@ for i in range(1, MAX):
     for j in range(i, MAX, i):
         fac[j].append(i)
 
-print(fac[6])
-print(fac[100])
-print(fac[121])
+print("fac :",fac[6])
+print("fac :",fac[100])
+print("fac :",fac[121])
 
 # classic : 3447. Assign Elements to Groups with Constraints
 # https://leetcode.com/problems/assign-elements-to-groups-with-constraints/description/
 
+# identify all prime numbers
+# MAX = 10 ** 5 + 1
+MAX = 100
+prime = [True]*MAX
+prime[0] = False
+prime[1] = False
+for i in range(2, int(MAX**(1/2)+1)):
+    if not prime[i]:
+        continue
+    for j in range(i*i, MAX, i):
+        prime[j] = False
+
+print("Prime numbers up to", MAX, ":")
+for i in range(MAX):
+    if prime[i]:
+        print(i, end=' ')
